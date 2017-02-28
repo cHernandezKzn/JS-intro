9ccd10dcce8f6197087cb007513e3c107e8483f5
Carlos Hernandez <carlosh95@gmail.com>
2017-02-28 16:14:57 -0500

    main testing setup done

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,18 @@
 const expect = require('chai').expect;
 const myModule = require('./index');
 
-describe('...', () => {
-  it('should ....', () => {
+describe('index main function', () => {
+  it('should exist', () => {
     expect(myModule.test).to.exist;
     expect(myModule.test).be.a.function;
+  });
+
+  it('should return Hello World', () => {
+    expect(myModule.test()).to.equal('Hello World');
+  });
+
+  it('should return Hello World', () => {
+    expect('hello').to.not.be.equal('ahello');
   });
 });
 
